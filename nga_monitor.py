df8973341eb9a3e8e49e4a27f4c9e6a792c17e12
milakakok <2256163155@qq.com>
2026-03-06 00:56:55 +0800

    Update nga_monitor.py

--- nga_monitor.py
+++ nga_monitor.py
@@ -4,9 +4,10 @@ import json
 import os
 
 # ====================== 只改这里 ======================
-WATCH_UIDS = ["66662897", "557398"，"150058"，"26529713"]  # 监控的用户UID
+WATCH_UIDS = ["66662897", "557398","150058","26529713"]  # 监控的用户UID
 NGA_COOKIE = "ngacn0comUserInfo=%25D3%25EA%25C2%25E4%25C7%25E0%25C9%25BD%09%25E9%259B%25A8%25E8%2590%25BD%25E9%259D%2592%25E5%25B1%25B1%0939%0939%09%0910%0910400%094%090%090%0961_5%2C39_30%2C87_15; __ad_cookie_mapping_tck_731=09371410aea4c684abbd3c9ed2d889dc; ngaPassportUid=41085401; ngaPassportUrlencodedUname=%25D3%25EA%25C2%25E4%25C7%25E0%25C9%25BD; ngaPassportCid=X9htfm15jqnes8bd33hfiipem0hbmaenllt9ie21; ngacn0comUserInfoCheck=bb6f89d844061915299b2929d7e2fceb; ngacn0comInfoCheckTime=1772636233; Hm_lvt_6933ef97905336bef84f9609785bcc3d=1772586354,1772600547,1772632124,1772715978; HMACCOUNT=CE51ACA11DEACE73; lastvisit=1772720444; lastpath=/read.php?tid=45502551&page=467; bbsmisccookies=%7B%22uisetting%22%3A%7B0%3A1%2C1%3A1773199973%7D%2C%22pv_count_for_insad%22%3A%7B0%3A-186%2C1%3A1772730074%7D%2C%22insad_views%22%3A%7B0%3A2%2C1%3A1772730074%7D%7D; Hm_lpvt_6933ef97905336bef84f9609785bcc3d=1772720690"         # 从Edge复制的Cookie
 DINGTALK_WEBHOOK = "https://oapi.dingtalk.com/robot/send?access_token=1985f68a2fc3fc59d343a563747dc79088375b00c694a6f4267154f24c972da0"
+
 # ======================================================
 
 def get_username(uid, headers):
